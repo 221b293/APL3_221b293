c36ab899bca4d005fb828b9d62146a0fb5a6b75e
221B293 <221B293@juet.co.in>
2025-01-22 15:39:09 +0530

    Updation in le_1->Q4

--- LE_1/Q4/One.java
+++ LE_1/Q4/One.java
@@ -2,17 +2,17 @@ class One{
      One(int x) {
         System.out.println("constructor of class 1 is called: "+ x);
     }
-    
-     static class Two extends One{
+}
+     class Two extends One{
     Two() {
         super(10); 
         System.out.println("constructor of class 2 is called");
     }
     }
+    
+    class Application{
     public static void main(String []args){
          One obj = new One(5); 
         Two obj2 = new Two();
     }
 }
-
-
